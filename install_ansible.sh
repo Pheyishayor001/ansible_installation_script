12fe382fe3c9908c8dbf47d3c2e29bfd66976f87
feyi <pheyishayor001@gmail.com>
2024-11-29 07:50:29 +0100

    updated bash script

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,23 +2,23 @@
 # Script to install Python 3, pip, Ansible, and configure terminal and Ansible settings on an Amazon Linux server
 
 # Update system packages
-yum update -y
+sudo yum update -y
 
 # Install Python 3.8
-amazon-linux-extras install python3.8 -y
+sudo amazon-linux-extras install python3.8 -y
 
 # Install pip for Python 3.8
-python3.8 -m ensurepip --upgrade
+sudo python3.8 -m ensurepip --upgrade
 
 # Install Ansible using pip
-python3.8 -m pip install --upgrade pip
+sudo python3.8 -m pip install --upgrade pip
 python3.8 -m pip install ansible
 
 # Install boto3, botocore, and awscli
-python3.8 -m pip install boto3 botocore awscli
+#python3.8 -m pip install boto3 botocore awscli
 
 # Install the Amazon Ansible EC2 plugin
-#ansible-galaxy collection install amazon.aws
+ansible-galaxy collection install amazon.aws
 
 # Initialize a default Ansible configuration file
 ansible-config init --disabled > ansible.cfg
